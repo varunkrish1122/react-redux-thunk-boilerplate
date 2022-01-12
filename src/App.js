e83d8dde62ad7638b077b71c379274a9138556f1
Madhuvaran <vrnsr2884@gmail.com>
2022-01-12 12:08:49 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Typography from 'apollo-react/components/Typography';
+
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      I am boilerplate with React and Redux
+    </div>
+  );
+}
+
+export default App;
